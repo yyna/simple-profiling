f40a3f6c519c9ff6cbab15159483aa4a0592f461
Jungin Kwon <jungin.kwon1@gmail.com>
2023-06-23 01:30:17 +0900

    asynchronously

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const EventEmitter = require('eventemitter3');
+const jobEmitter = new EventEmitter();
+
+jobEmitter
+  .on('after-creating-something', async (payload) => {
+    await taskToBeProcessedAsynchronously('task after creating something');
+  })
+  .on('after-updating-something', async (payload) => {
+    await taskToBeProcessedAsynchronously('task after updating something');
+  });
+
 app.use(express.json());
 
 const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
@@ -14,13 +25,13 @@ const taskToBeProcessedAsynchronously = async (task) => {
 
 // create something
 app.post('/something', async (req, res) => {
-  await taskToBeProcessedAsynchronously('task after creating something');
+  jobEmitter.emit('after-creating-something');
   res.sendStatus(200);
 });
 
 // update something
 app.put('/something/:id', async (req, res) => {
-  await taskToBeProcessedAsynchronously('task after updating something');
+  jobEmitter.emit('after-updating-something');
   res.sendStatus(200);
 });
 
